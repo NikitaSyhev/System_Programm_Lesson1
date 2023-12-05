c8e0ef60b820105007604bc5edf103674ebb1bbf
Nikita Sychev <nikitasychev@inbox.ru>
2023-12-05 19:51:48 +0500

    Add project files.

---
+++ ClassLibrary/Math/MyMath.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class MyMath
+    {
+        public static bool IsLeap(int year)
+        {        
+
+            return (year % 4 == 0) && ((year % 100 != 0 )||(year % 400 == 0));
+        }
+        public static bool IsLeap(DateTime year) 
+        {   
+            return IsLeap(year.Year);
+        }
+        public static bool IsLeap(string year)
+        {
+            
+            if ((year.Contains('.'))||(year.Contains('/')))
+                return IsLeap(Convert.ToDateTime(year).Year);
+            else
+                return IsLeap(Convert.ToInt32(year));
+        }
+        public static decimal Summ(decimal a, decimal b, decimal c)
+            { return a + b + c; }
+        public static decimal Summ(string x)
+        {
+            decimal result = 0;
+            string[] numbers = x.Split(';');
+            foreach (string number in numbers) 
+                { result += Convert.ToDecimal(number); }
+            return result; 
+        }
+
+        public static decimal Max(decimal a, decimal b, decimal c) 
+        {
+            return new[] { a, b, c }.Max();
+        }
+        public static decimal Max(string x)
+        {
+            decimal[] a =x.Split(';').Select(y => decimal.Parse(y)).ToArray();
+            return a.Max();
+        }
+        public static decimal Min(decimal a, decimal b, decimal c)
+        {
+            return new[] { a, b, c }.Min();
+        }
+        public static decimal Min(string x)
+        {
+            decimal[] a = x.Split(';').Select(y => decimal.Parse(y)).ToArray();
+            return a.Min();
+        }
+    }
+}
---
+++ TestDLLsForms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+
+
+namespace TestDLLsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MyMath.IsLeap(Msg.Text))
+                ClassLibrary.Message.GetMessage("Високосный");
+            else
+                ClassLibrary.Message.GetMessage("Не високосный"); ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var d=MyMath.Max(Msg.Text);
+        }
+    }
+}
